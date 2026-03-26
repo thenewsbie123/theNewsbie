07fdfe82954aa28fee6c99a022ed351550207d02
thenewsbie123 <thenewsbieteam1@gmail.com>
2026-03-26 14:14:11 +0530

    change files

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 // config/db.js
 // This file connects your app to MongoDB using Mongoose.
 
+// config/db.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-     await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`🟢 MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
-    process.exit(1); // Stop the server if DB fails to connect
+    process.exit(1);
   }
 };
 
