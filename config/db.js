b5bacb3d829fdd6b2e2d9812d30621c178e7d926
thenewsbie123 <thenewsbieteam1@gmail.com>
2026-03-26 15:06:06 +0530

    mkc555555

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`🟢 MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
